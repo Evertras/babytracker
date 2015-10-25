a5c07ee2006ff4709c06de28cdedc1689a8fdb71
evertras <bfullj@gmail.com>
2015-10-25 03:33:52 -0400

    Starting to add in API

--- server/server.js
+++ server/server.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.get('/api/', function (req, res) {
-    res.send("Hi");
+var tasks = {};
+
+app.get('/api/task/:task', function (req, res) {
+    if (!tasks[req.params.task]) {
+        tasks[req.params.task] = {};
+    }
+
+    res.json(tasks[req.params.task]);
+});
+
+app.post('/api/task/:task/subtask', function (req, res) {
+    if (tasks[req.params.task]) {
+    } else {
+    }
+
+    res.send();
 });
 
 app.use(express.static('wwwroot'));
