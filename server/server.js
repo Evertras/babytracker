ff95f8f0425eb34083ac2f228d08ea0643b6afd1
evertras <bfullj@gmail.com>
2015-10-26 02:39:03 -0400

    More API work

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var moment = require('moment');
 
 var app = express();
 
 var tasks = {};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/task/', function (req, res) {
+    res.json(tasks);
+});
 
 app.get('/api/task/:task', function (req, res) {
     if (!tasks[req.params.task]) {
@@ -12,10 +21,22 @@ app.get('/api/task/:task', function (req, res) {
     res.json(tasks[req.params.task]);
 });
 
-app.post('/api/task/:task/subtask', function (req, res) {
-    if (tasks[req.params.task]) {
-    } else {
+app.post('/api/task/:task/', function (req, res) {
+    tasks[req.params.task] = {
+        'Diaper': generateSubtask(moment.duration)
+    };
+
+    res.send();
+});
+
+app.post('/api/task/:task/subtask/', function (req, res) {
+    console.log(req.body);
+
+    if (!tasks[req.params.task]) {
+        tasks[req.params.task] = {};
     }
+
+    tasks[req.params.task][req.body.Name] = req.body;
 
     res.send();
 });
@@ -25,3 +46,9 @@ app.use(express.static('wwwroot'));
 var server = app.listen(4321, function() {
     console.log("Started!");
 });
+
+function generateSubtask(secondsTotal) {
+    return {
+        targetTime: moment().add(secondsTotal, 'seconds')
+    };
+}
